7c2bd8fe24ecbc190ff5dc7a8596b7db5302b7ac
lordluci4er <lordluci4er@gmail.com>
2026-05-03 03:17:04 +0530

    last stable version restored

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -3,6 +3,8 @@ import { googleAuth } from "../controllers/authController.js";
 
 const router = express.Router();
 
+/// 🔥 GOOGLE AUTH LOGIN / REGISTER
+// POST /api/auth/google
 router.post("/google", googleAuth);
 
 export default router;
